0dc50544e609bb4b3804ff212648c1d55d9a7887
vishwakarmasachin68 <vksachin86@gmail.com>
2025-07-30 12:15:30 +0530

    Hosting in render

--- database.py
+++ database.py
@@ -1,11 +1,23 @@
+# from sqlalchemy import create_engine 
+# from sqlalchemy.orm import sessionmaker, declarative_base 
+
+# DATABASE_URL = "postgresql://postgres:7410@localhost:5432/dc_generator_db" 
+
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# Base = declarative_base()
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:7410@localhost:5432/dc_generator_db" 
-# DATABASE_URL="postgresql://postgres:7410@localhost:5432/delivery_challan_final_backend"
- # Change as required
+# Get DATABASE_URL from environment; fallback only for local dev
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:7410@localhost:5432/dc_generator_db"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
-
